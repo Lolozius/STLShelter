25ca2e22b6c1d7c9bf2c293d7b7fc2fd2d9678cc
Lolozius <61847373+Lolozius@users.noreply.github.com>
2023-07-20 22:41:58 +0300

    Create form, create model, views wip

--- shelter/sheel/views.py
+++ shelter/sheel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from .forms import PostForm
 from django.http import HttpResponse
 
 
@@ -7,4 +8,13 @@ def index(request):
 
 
 def post(request):
-    return render(request, 'page/create_post.html')
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+    )
+    if not form.is_valid():
+        return render(request, 'page/create_post.html', {'form': form})
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect('page/index.html')
